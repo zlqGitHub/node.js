073ceabf56dae8f7a23d8cc0bd60126b570ef4c7
张利强 <1509874021@qq.com>
2019-05-23 09:38:01 +0800

    初始node.js

--- code1/01.js
+++ code1/01.js
@@ -1,2 +1,11 @@
+/*
+    注意：node是基于 v8 引擎，可以在终端cmd中输入node后，直接编写js代码
+    在终端如何运行 js 文件呢：
+        1.找到要执行js文件对应的目录
+        2.在终端输入： node ***.js
+    还可以在文件夹中，直接输入 cmd ，打开当前文件对应的终端
+*/
+
 console.log("Hello World");
 console.log("hahaha");
+
